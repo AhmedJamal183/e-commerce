fa4beda2a9e48c262b6d9e500e0bf25a76e6c099
Ahmed Jamal <ahmed.jamal@motorolasolutions.com>
2024-08-29 17:28:58 -0500

    added query to create schema and tables

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 const express = require('express')
+const { Pool } = require('pg'); 
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'postgres',
+    port: 5432,
+  });
+  
+  app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  async function createSchema() {
+    try {
+      const query = `
+        CREATE SCHEMA IF NOT EXISTS "e-commerce";
+      `;
+  
+      await pool.query(query);
+      console.log('E-Commerce schema created');
+    } catch (err) {
+      console.error(err);
+      console.error('E-Commerce schema creation failed');
+    }
+  }
+
+  async function createProductsTable() {
+    try {
+      const query = `
+        CREATE TABLE IF NOT EXISTS "e-commerce"."products" (
+          productId SERIAL PRIMARY KEY,
+          productTitle VARCHAR(255) NOT NULL,
+          productPrice NUMERIC(10, 2)
+        );
+      `;
+  
+      await pool.query(query);
+      console.log('Products table created');
+    } catch (err) {
+      console.error(err);
+      console.error('Products table creation failed');
+    }
+  }
+
+  async function createOrdersTable() {
+    try {
+      const query = `
+        CREATE TABLE IF NOT EXISTS "e-commerce"."orders" (
+          orderId SERIAL PRIMARY KEY,
+          productId INTEGER REFERENCES "e-commerce"."products" (productId),
+          orderQuantity INTEGER
+        );
+      `;
+  
+      await pool.query(query);
+      console.log('Orders table created');
+    } catch (err) {
+      console.error(err);
+      console.error('Orders table creation failed');
+    }
+  }
+  
+  createSchema();
+  createProductsTable();
+  createOrdersTable();
+  
